b5347e82013124e90e9972bbcb7d3f20c51f47c4
W4LK3RZ1NH0 <lucasgalveias@gmail.com>
2024-12-06 10:29:33 +0000

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(name='lucas_galveias',
+      version='1.0',
+      description='Lucas Galveias Distribution Module',
+      author='W4LK3R',
+      author_email='lucasgalveias@gmail.com',
+      url='The URL project - github for example',
+      py_modules=['lucas_galveias'],
+     )
+#!pip install --no-index C:\Users\lucas\Desktop\Projetos_Uni\Jupyter\Python\MyModules\lucas_galveias
